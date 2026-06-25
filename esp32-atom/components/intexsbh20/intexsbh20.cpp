58887f886644da4d43dc39376b4b1543f60676f9
nalditopr <19629225+nalditopr@users.noreply.github.com>
2026-06-25 10:59:56 -0500

    Add frame-count diagnostic log to update() (is the ISR receiving?)

--- esp32-atom/components/intexsbh20/intexsbh20.cpp
+++ esp32-atom/components/intexsbh20/intexsbh20.cpp
@@ -17,6 +17,9 @@ void IntexSBH20::loop()
 
 void IntexSBH20::update()
 {
+	ESP_LOGD("intexsbh20", "diag: frames=%u dropped=%u online=%d power=%u ledRaw=0x%04X",
+	         sbh_.getTotalFrames(), sbh_.getDroppedFrames(), (int) sbh_.isOnline(), sbh_.isPowerOn(), sbh_.getRawLedValue());
+
 	int errorValue = sbh_.getErrorValue();
 	if (errorValue != 0)
 	{
